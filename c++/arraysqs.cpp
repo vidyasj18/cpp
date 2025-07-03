33886533ed9748194ddba5c2d57ff1f0a532f829
vidya s j <vidyajamkhandi1@gmail.com>
2025-07-03 17:55:04 +0530

    arrays qs

--- c++/arraysqs.cpp
+++ c++/arraysqs.cpp
@@ -122,4 +122,35 @@ public:
     }
 };
 
+// qs - 4 : Rotate matrix - leetcode
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+
+        // vertical reversal 
+        for(int j = 0; j<n; j++) {
+            int s = 0;
+            int e = n-1;
+
+            while(s<e) {
+                swap(matrix[s][j],matrix[e][j]);
+                s++;
+                e--;
+            }
+
+        }
+
+        // Transpose
+        for(int i = 0; i<n; i++) {
+            for(int j = i+1; j<n; j++) {
+                if(i!=j) {
+                    swap(matrix[i][j],matrix[j][i]);
+                }
+            }
+        }
+    }
+};
+
 
